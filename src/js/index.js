0e4197442fbd422cb9a8de9cd3d920a8640740e7
Tarkanii Artur <arturmandragon@gmail.com>
2024-12-17 16:16:57 +0000

    DM-5, added about us section

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import '../sass/main.scss';
 import menuHandler from './menu';
+import readMoreHandler from './read-more';
 
 document.addEventListener('DOMContentLoaded', () => {
     menuHandler();
+    readMoreHandler();
 })
