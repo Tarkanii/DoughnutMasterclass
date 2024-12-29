77b23db433a6661fd4272658a4b6c6afc9fda8d3
Tarkanii Artur <arturmandragon@gmail.com>
2024-12-29 22:50:29 +0000

    DM-7, added reviews section

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
 import '../sass/main.scss';
 import menuHandler from './menu';
 import readMoreHandler from './read-more';
+import sliderHandler from './slider';
 
 document.addEventListener('DOMContentLoaded', () => {
     menuHandler();
     readMoreHandler();
+    sliderHandler();
 })
